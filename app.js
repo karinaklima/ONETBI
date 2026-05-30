2cfc942fd4303ee2595ab1ee1da7696b4ed05a5a
Karinaklima <karinakarla.lima@hotmail.com>
2026-05-30 10:41:26 -0300

    Atualização do portal BI

---
+++ app.js
@@ -0,0 +1,48 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async function(event){
+
+    event.preventDefault();
+
+    const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+
+    try{
+
+        const resposta = await fetch("http://localhost:3000/login", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                usuario,
+                senha
+            })
+
+        });
+
+        const dados = await resposta.json();
+
+        if(dados.sucesso){
+            sessionStorage.setItem("logado", "true");
+            
+            window.location.href = "dash.html";
+
+        }else{
+
+            alert(dados.mensagem);
+
+        }
+
+    }catch(error){
+
+        alert("Erro ao conectar com servidor");
+
+        console.log(error);
+
+    }
+
+});
